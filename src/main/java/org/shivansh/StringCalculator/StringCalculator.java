056638fd9445ef0cdc105ea476cae8cc42c1a303
shivansh singh <shivanshjobboy@gmail.com>
2024-09-11 14:47:50 +0530

    Fixed add() method from StringCalculator class to handle empty String Edge case,though for value other than Empty String it will still fail

--- src/main/java/org/shivansh/StringCalculator/StringCalculator.java
+++ src/main/java/org/shivansh/StringCalculator/StringCalculator.java
@@ -2,6 +2,13 @@ package org.shivansh.StringCalculator;
 
 public class StringCalculator {
   public int add(String numbers){
+    if(numbers.isEmpty()){
+      return 0;
+    }
+
+    /*
+    temporary default value ,will be change once updated.
+     */
     return Integer.MAX_VALUE;
   }
 }
