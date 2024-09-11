d431bc8920a9e75680ac67db2397d1a5b79ac397
shivansh singh <shivanshjobboy@gmail.com>
2024-09-11 15:47:24 +0530

    enhanced add() method of StringCalculator to support new line test case ,along with previous functionality.

--- src/main/java/org/shivansh/StringCalculator/StringCalculator.java
+++ src/main/java/org/shivansh/StringCalculator/StringCalculator.java
@@ -1,23 +1,25 @@
 package org.shivansh.StringCalculator;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class StringCalculator {
-  public int add(String numbers){
-          if(numbers.isEmpty()){
-              return 0;
-          }
-     /*
-     here we converted the comma separated string to array of strings
-      */
-      String[]numberStrings = numbers.split(",");
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
 
-    /*
-   returning sum of all the number strings
-   */
-      return Arrays.stream(numberStrings).mapToInt(Integer::parseInt).sum();
+        /*
+         * Split the input string by both commas and new lines.
+         * The regular expression ",|\n" matches either a comma or a new line.
+         * This handles cases where numbers are separated by commas or new lines.
+         */
+        String[] numberStrings = numbers.split(",|\n");
 
-      }
-  }
-
+        /*
+         * returning the sum of all numbered strings
+         */
+        return Arrays.stream(numberStrings)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+}
